14707d0e799060722edeb8a7e77c516735d39d44
Itzdlg <dontyoutrybrah@gmail.com>
2023-01-22 21:02:29 -0500

    Fix SectionUtils#first() returning null/incorrect sections.

--- src/main/java/me/epic/betteritemconfig/SectionUtils.java
+++ src/main/java/me/epic/betteritemconfig/SectionUtils.java
@@ -2,19 +2,25 @@ package me.epic.betteritemconfig;
 
 import org.bukkit.configuration.ConfigurationSection;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
-public class SectionUtils {
+public final class SectionUtils {
+    private SectionUtils() { }
 
     public static ConfigurationSection first(ConfigurationSection parent, String... kids) {
-        List<String> children = Arrays.asList(kids).stream().map(kid -> kid.toLowerCase(Locale.ROOT)).toList();
-        return parent.getKeys(false).stream()
+        List<String> sections = parent.getKeys(false).stream()
                 .filter(parent::isConfigurationSection)
-                .map(key -> key.toLowerCase(Locale.ROOT)) /* no Locale.ITALY :pepe_cry_hands: */
-                .filter(children::contains)
-                .map(parent::getConfigurationSection)
-                .findFirst().orElse(null);
+                .toList();
+
+        for (String kid : kids) {
+            for (String section : sections) {
+                if (!kid.equalsIgnoreCase(section))
+                    continue;
+
+                return parent.getConfigurationSection(section);
+            }
+        }
+
+        return null;
     }
 }
